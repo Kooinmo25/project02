9fdd651a6f3f04a074d8e5b18b911d015369f1f1
osh2004 <neosky1448@gmail.com>
2024-05-07 17:49:27 +0900

    category practice

--- src/component/Category.js
+++ src/component/Category.js
@@ -1,51 +1,31 @@
-import Table from 'react-bootstrap/Table';
+import Accordion from 'react-bootstrap/Accordion';
 
 function Category() {
     return (
-        <div>
-            <Table responsive="sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                    </tr>
-                </tbody>
-            </Table>
-        </div>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>성별</Accordion.Header>
+                <Accordion.Body>
+                    남성 여성
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>브랜드</Accordion.Header>
+                <Accordion.Body>
+                    아레나 나이키 르망고 배럴 후그 레노마 센티 키치피치 스웨이브 비키니밴더 엘르
+                    스피도 아디다스 미즈노 랠리 아쿠아티카 티어 나이키스윔 제테스 723후그 엑스블루
+                    제이커스 토네이도 버버리 베이직엘르 캘빈클라인 센티 티막 펑키타 아날도바시니
+                </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="2">
+                <Accordion.Header>가격</Accordion.Header>
+                <Accordion.Body>
+                    2만원~3만원 3만원~5만원 5만원~7만원 7만원~10만원
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
     );
 }
 
