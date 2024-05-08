e16b473401c3fe6f787372ed238714f9f47120e1
KooInMo(grsn) <moinmoinkoo@naver.com>
2024-05-08 12:35:59 +0900

    홈 페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Layout from "./component/common/Layout";
+/*eslint-disable*/
 
+import Footer from "./component/common/Footer";
+import Layout from "./component/common/Layout";
 
 function App() {
   return (
