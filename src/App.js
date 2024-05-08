b461d64571612eddd26cdf4be789e3d42a1cb301
osh2004 <neosky1448@gmail.com>
2024-05-08 10:27:57 +0900

    Category event handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 
 import Category from './component/Category';
-import Get from './component/Get';
 
 function App() {
   return (
     <>
       <Category />
-      <Get />
     </>
   );
 }
--- src/component/Category.js
+++ src/component/Category.js
@@ -1,28 +1,34 @@
+import { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 
 function Category() {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
+    // 카테고리 클릭 핸들러
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
     return (
         <Accordion defaultActiveKey="0">
             <Accordion.Item eventKey="0">
                 <Accordion.Header>성별</Accordion.Header>
                 <Accordion.Body>
-                    남성 여성
+                    <button onClick={() => handleCategoryClick('성별')}>성별</button>
                 </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1">
                 <Accordion.Header>브랜드</Accordion.Header>
                 <Accordion.Body>
-                    아레나 나이키 르망고 배럴 후그 레노마 센티 키치피치 스웨이브 비키니밴더 엘르
-                    스피도 아디다스 미즈노 랠리 아쿠아티카 티어 나이키스윔 제테스 723후그 엑스블루
-                    제이커스 토네이도 버버리 베이직엘르 캘빈클라인 센티 티막 펑키타 아날도바시니
+                    <button onClick={() => handleCategoryClick('브랜드')}>브랜드</button>
                 </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="2">
                 <Accordion.Header>가격</Accordion.Header>
                 <Accordion.Body>
-                    2만원~3만원 3만원~5만원 5만원~7만원 7만원~10만원
+                    <button onClick={() => handleCategoryClick('가격')}>가격</button>
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
