066ad7e47dd3389c5bd6c7809413a0ef16ff2625
ysw1208 <tony605a@naver.com>
2024-05-08 12:41:24 +0900

    장바구니 수정

--- src/component/shoppingcart/Value.js
+++ src/component/shoppingcart/Value.js
@@ -5,13 +5,15 @@ function removebtag(text) {
 }
 function ValueReturn(props) {
 
+    const titledel = removebtag(props.title);
+
     return (
         <Stack direction="horizontal" gap={3}>
             <div className="p-2">
                 <img src={props.image} style={{ width: "30%", height: "auto" }}></img>
             </div>
-            <div className="p-2" style={{ fontSize: "18px", color: "#333" }}>{props.title}</div>
-            <div className="p-2" style={{ fontSize: "18px", color: "green" }}>{props.price}</div>
+            <div className="p-2" style={{ fontSize: "18px", color: "#333" }}>{titledel}</div>
+            <div className="p-2" style={{ fontSize: "18px", color: "black" }}>{props.price}</div>
         </Stack>
     );
 }
