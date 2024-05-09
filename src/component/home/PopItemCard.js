8d96614134935eaed4bbbd5ea6d4b0f7381466c8
KooInMo(grsn) <moinmoinkoo@naver.com>
2024-05-09 17:05:19 +0900

    홈 수정

--- src/component/home/PopItemCard.js
+++ src/component/home/PopItemCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import { useMediaQuery } from 'react-responsive';
@@ -17,10 +17,6 @@ function PopItemCard(props) {
     const titleDel = removebtag(props.title);
     const commas = addCommas(props.price)
 
-    const handleSlideButtonClick = () => {
-        // 슬라이드 좌우 이동 함수 호출
-    };
-
     return (
         <Col className='d-flex justify-content-center align-items-center'>
             <Card style={{ width: isMobile ? '15rem' : '30rem' }}>
@@ -29,13 +25,13 @@ function PopItemCard(props) {
                     <Card.Title className="text-truncate">{titleDel}</Card.Title>
                     <Card.Text style={{ fontWeight: "bold", fontSize: "20px" }} className="d-flex justify-content-between">
                         <span>{commas} 원</span>
-                        <div>
-                            <Button className='mx-2'>구매하기</Button>
-                            <Button>장바구니에 담기</Button>
-                        </div>
+                        <ButtonGroup>
+                            <Button variant = "secondary">구매하기</Button>
+                            <Button variant='secondary'>장바구니에 담기</Button>
+                        </ButtonGroup>
                     </Card.Text>
                 </Card.Body>
-                <Button variant='primary' onClick={handleSlideButtonClick}>상세정보</Button>
+                <Button variant='primary'>상세정보</Button>
             </Card>
         </Col>
     );
