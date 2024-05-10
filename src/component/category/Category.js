371d58e7d60a3c463deb8725077cc0747b9b857e
osh2004 <neosky1448@gmail.com>
2024-05-10 11:05:07 +0900

    color add

--- src/component/category/Category.js
+++ src/component/category/Category.js
@@ -17,8 +17,11 @@ function Category() {
         ['아레나', '나이키', '르망고', '배럴', '후그', '센티', '키치피치', '스웨이브', '비키니밴더', '엘르', '토네이도', '미즈노', '발리비키', '티막', '나인에프엑스', '아디다스', '리복'
             , '스피도', 'UNKNOWN', '랠리', '레노마', '아쿠아티카', '티어', '제테스', '723후그', '엑스블루', '제이커스', '버버리', '캘빈클라인', '펑키타', '르망고', '아날도바시니', '쿠기'
             , '제인코트', 'GANNI', 'H&M', '스키즈', 'TOTEME', '데이즈데이즈', '닉스원', '필로드', '헤링본', '돌핀', '티에스나인', '에르메스', '오스카', '알라이아 컷아웃', '샤넬', '구찌'],
+        ['빨강', '파랑', '초록', '노랑', '주황', '보라', '아이보리', '민트', '핑크', '화이트', '블랙'],
         ['1만원 ~ 2만원', '2만원 ~ 4만원', '4만원 ~ 8만원']
     ];
+
+    const colorClasses = ['red-button', 'blue-button', 'green-button', 'yellow-button', 'orange-button', 'purple-button', 'ivory-button', 'mint-button', 'pink-button', 'white-button', 'black-button'];
 
     return (
         <>
@@ -48,12 +51,24 @@ function Category() {
                     </Accordion.Body>
                 </Accordion.Item>
 
-                {/* 가격 */}
+                {/* 색상 */}
                 <Accordion.Item eventKey="2">
+                    <Accordion.Header className="categoryHeader">색상</Accordion.Header>
+                    <Accordion.Body className="categoryBody">
+                        <div className="button-container">
+                            {buttons[2].map((text, index) => (
+                                <button className={`button ${colorClasses[index % colorClasses.length]}`} onClick={() => handleCategoryClick(text)}>{text}</button>
+                            ))}
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                {/* 가격 */}
+                <Accordion.Item eventKey="3">
                     <Accordion.Header className="categoryHeader">가격</Accordion.Header>
                     <Accordion.Body className="categoryBody">
                         <div className="button-container">
-                            {buttons[2].map((text) => (
+                            {buttons[3].map((text) => (
                                 <button className="button" onClick={() => handleCategoryClick(text)}>{text}</button>
                             ))}
                         </div>
