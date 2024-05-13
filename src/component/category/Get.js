d8a0d9bd856205f5f06eeabfeab800d6a10f41ae
osh2004 <neosky1448@gmail.com>
2024-05-13 09:55:59 +0900

    list test

--- src/component/category/Get.js
+++ src/component/category/Get.js
@@ -3,14 +3,20 @@ import { Col, Row } from "react-bootstrap";
 import ProductList from "../productList/ProductList";
 
 function Get({ brand }) {
-    const [list, setlist] = useState([]);
+    const [list, setList] = useState([]);
     const clientId = "C88k7kKQEPtcbHOYYaRs";
     const clientSecret = "5XoMjg7Tdx";
 
     useEffect(() => {
-        if (brand) { // 브랜드 값이 있을 때만 실행
-            fetch(
-                `/v1/search/shop?query=${brand}수영복&filter=used:false&sort=sim&display=100&start=1`, {
+        if (brand) {
+            let url;
+            if (typeof brand === 'string') {
+                url = `/v1/search/shop?query=${brand}수영복&filter=used:false&sort=sim&display=100&start=1`;
+            } else if (brand.min && brand.max) {
+                url = `/v1/search/shop?query=수영복&filter=used:false&sort=sim&display=100&start=1&min=${brand.min}&max=${brand.max}`;
+            }
+
+            fetch(url, {
                 method: "GET",
                 headers: {
                     "X-Naver-Client-Id": clientId,
@@ -18,9 +24,10 @@ function Get({ brand }) {
                 },
             })
                 .then((response) => response.json())
-                .then((json) => setlist(json.items));
+                .then((json) => setList(json.items))
+                .catch((error) => console.error('Error fetching data:', error));
         }
-    }, [brand]); // brand 값이 변경될 때마다 실행
+    }, [brand]);
 
     return (
         <Row xs={1} md={2} lg={4} className="g-4">
